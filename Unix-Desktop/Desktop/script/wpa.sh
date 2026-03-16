476aa889a07bfcda37c3cff83f92db4211bbeeba
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2026-03-16 18:46:24 +0000

    Update wpa.sh

--- Unix-Desktop/Desktop/script/wpa.sh
+++ Unix-Desktop/Desktop/script/wpa.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-IFACE="wlan0"
+# Scan
+wpa_cli -i wlan0 scan
+sleep 3
 
-# Escanear redes
-SCAN=$(wpa_cli -i $IFACE scan >/dev/null 2>&1 && sleep 2 && \
-wpa_cli -i $IFACE scan_results | awk 'NR>2 {print $5}' | sort -u)
-
-# Seleccionar red
-SSID=$(echo "$SCAN" | yad --list \
---title="WiFi Scanner" \
---column="SSID" \
---height=300 --width=300)
+# Show networks
+SSID=$(wpa_cli -i wlan0 scan_results | awk 'NR>2 {print $5}' | sort -u | \
+yad --list --title="WiFi" --column="SSID" --height=300 --width=300)
 
 [ -z "$SSID" ] && exit
 
-# Pedir password
-PASS=$(yad --entry \
---title="Password" \
---text="Password for $SSID" \
---hide-text)
+# Ask password
+PASS=$(yad --entry --title="Password" --text="Password for $SSID" --hide-text)
 
 [ -z "$PASS" ] && exit
 
-# Conectar
-NETID=$(wpa_cli -i $IFACE add_network | tail -n1)
+# Add network
+ID=$(wpa_cli -i wlan0 add_network | tail -n1)
 
-wpa_cli -i $IFACE set_network $NETID ssid "\"$SSID\"" >/dev/null
-wpa_cli -i $IFACE set_network $NETID psk "\"$PASS\"" >/dev/null
-wpa_cli -i $IFACE enable_network $NETID >/dev/null
+# Set parameters
+wpa_cli -i wlan0 set_network $ID ssid "\"$SSID\""
+wpa_cli -i wlan0 set_network $ID psk "\"$PASS\""
 
-# Mostrar estado
-xterm -e "wpa_cli -i $IFACE status; echo; echo 'Press Enter to close'; read"
+# Connect
+wpa_cli -i wlan0 enable_network $ID
+
+# Status
+xterm -e "wpa_cli -i wlan0 status; read"
